87b48f2dd71b7e873c3c06f5098f0ff38d5b2c2d
Alexey <kulakovalexey@proton.me>
2022-07-03 19:46:15 +0300

    fixed shapes

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-let isModalOpen = false
-let contrastToggle = false
-const scaleFactor = 1 / 20
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape")
-  const x = event.clientX * scaleFactor
-  const y = event.clientY * scaleFactor
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
 
   for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0
-    const boolInt = isOdd ? -1 : 1
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
     shapes[i].style.transform = `translate(${x * boolInt}px, ${
-      y * boolInt} px) rotate(${x * boolInt * 10}deg)`
+      y * boolInt}px) rotate(${x * boolInt * 10}deg)`;
   }
 }
 
 function toggleContrast() {
-  contrastToggle = !contrastToggle
+  contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
+    document.body.classList += " dark-theme";
   } else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList.remove("dark-theme");
   }
 }
 
 function contact(event) {
-  event.preventDefault()
-  const loading = document.querySelector(".modal__overlay--loading")
-  const success = document.querySelector(".modal__overlay--success")
-  loading.classList += " modal__overlay--visible"
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
       "service_tuxn379",
@@ -37,22 +37,31 @@ function contact(event) {
       "5AUAEuPdtFJ5aGob4"
     )
     .then(() => {
-      loading.classList.remove("modal__overlay--visible")
-      success.classList += " modal__overlay--visible"
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
     })
     .catch(() => {
-      loading.classList.remove("modal__overlay--visible")
+      loading.classList.remove("modal__overlay--visible");
       alert(
         "The email service is temporarily unavailable. Please contact me directly at kulakovalexey@proton.me"
-      )
-    })
+      );
+    });
 }
 
 function toggleModal() {
   if (isModalOpen) {
-    isModalOpen = false
-    return document.body.classList.remove("modal--open")
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
   }
-  isModalOpen = true
-  document.body.classList += " modal--open"
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
+
+function sortArray(arr) {
+  let flag = true;
+  arr.forEach((element, index) => {});
+
+  return flag;
+}
+
+console.log(sortArray([1, 2, 3, 4, 5, 6, 7]));
